0bd3e925809e46ab67af38387b7f92bcb2021cfe
Tom Wright <contact@tomwright.me>
2022-05-06 17:18:49 +0100

    Add a test

--- repository_test.go
+++ repository_test.go
@@ -125,27 +125,156 @@ func TestRepository_QueryRow(t *testing.T) {
 				return
 			}
 
-			numColumns := len(query.Fields)
-			columns := make([]string, numColumns)
-			for index, column := range query.Fields {
-				columns[index] = string(column)
-			}
-
-			values := make([]any, numColumns)
-			valuePointers := make([]any, numColumns)
-			for k := range values {
-				valuePointers[k] = &values[k]
-			}
-
-			if err := row.Scan(valuePointers...); err != nil {
-				t.Errorf("could not scan row: %v", err)
+			got, err := scanRowToMapInterface(query.Fields, row)
+			if err != nil {
+				t.Error(err)
 				return
 			}
 
-			got := make(map[string]any)
+			if !checkDiff(t, tc.exp, got) {
+				return
+			}
+		})
+	}
+}
 
-			for i, column := range columns {
-				got[column] = values[i]
+func TestRepository_Query(t *testing.T) {
+	type def struct {
+		name         string
+		table        string
+		selectFields []qry.Field
+		query        func() qry.SelectQuery
+		exp          []map[string]any
+		mockFn       func(db sqlmock.Sqlmock)
+	}
+	tests := []def{
+		{
+			name:         "Fallback to defaults",
+			table:        "users",
+			selectFields: []qry.Field{"id", "name"},
+			query: func() qry.SelectQuery {
+				query := qry.Select()
+				return query
+			},
+			exp: []map[string]any{
+				{
+					"id":   int64(1),
+					"name": "Tom",
+				},
+				{
+					"id":   int64(2),
+					"name": "Jim",
+				},
+			},
+			mockFn: func(db sqlmock.Sqlmock) {
+				db.ExpectPrepare("SELECT id, name FROM users").
+					ExpectQuery().
+					WillReturnRows(
+						sqlmock.NewRows([]string{"id", "name"}).
+							AddRow(int64(1), "Tom").
+							AddRow(int64(2), "Jim"),
+					).
+					RowsWillBeClosed()
+			},
+		},
+		{
+			name:         "Basic select",
+			table:        "users",
+			selectFields: []qry.Field{"id", "name"},
+			query: func() qry.SelectQuery {
+				query := qry.Select()
+				query.Table = "user"
+				query.Fields = []qry.Field{"uuid", "username"}
+				return query
+			},
+			exp: []map[string]any{
+				{
+					"uuid":     "11112222-3333-4444-5555-666677778888",
+					"username": "Tom",
+				},
+				{
+					"uuid":     "88887777-6666-5555-4444-333322221111",
+					"username": "Jim",
+				},
+			},
+			mockFn: func(db sqlmock.Sqlmock) {
+				db.ExpectPrepare("SELECT uuid, username FROM user").
+					ExpectQuery().
+					WillReturnRows(
+						sqlmock.NewRows([]string{"uuid", "username"}).
+							AddRow("11112222-3333-4444-5555-666677778888", "Tom").
+							AddRow("88887777-6666-5555-4444-333322221111", "Jim"),
+					).
+					RowsWillBeClosed()
+			},
+		},
+		{
+			name: "Select with equal condition",
+			query: func() qry.SelectQuery {
+				query := qry.Select()
+				query.Table = "users"
+				query.Fields = []qry.Field{"id", "name"}
+				query.Condition = qry.Equal("id", 1)
+				return query
+			},
+			exp: []map[string]any{
+				{
+					"id":   int64(1),
+					"name": "Tom",
+				},
+			},
+
+			mockFn: func(db sqlmock.Sqlmock) {
+				db.ExpectPrepare("SELECT id, name FROM users WHERE id = ?").
+					ExpectQuery().
+					WithArgs(1).
+					WillReturnRows(
+						sqlmock.NewRows([]string{"id", "name"}).
+							AddRow(1, "Tom"),
+					).
+					RowsWillBeClosed()
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tc := test
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			if err != nil {
+				t.Errorf("could not init db mock")
+				return
+			}
+
+			defer func(db *sql.DB) {
+				_ = db.Close()
+			}(db)
+
+			tc.mockFn(mock)
+
+			var query qry.SelectQuery
+			repo := qry.Repository{
+				DB:                   db,
+				Table:                tc.table,
+				StandardSelectFields: tc.selectFields,
+				PreSelectFn: func(ctx context.Context, innerQuery qry.Query) error {
+					query = innerQuery.(qry.SelectQuery)
+					return nil
+				},
+			}
+
+			rows, err := repo.Query(context.Background(), tc.query())
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+				return
+			}
+
+			got, err := scanRowsToMapInterface(query.Fields, rows)
+			if err != nil {
+				t.Error(err)
+				return
 			}
 
 			if !checkDiff(t, tc.exp, got) {
--- utils_test.go
+++ utils_test.go
@@ -1,7 +1,9 @@
 package qry_test
 
 import (
+	"database/sql"
 	"fmt"
+	"github.com/TomWright/qry"
 	"github.com/go-test/deep"
 	"testing"
 )
@@ -23,4 +25,47 @@ func checkDiffMsg(t *testing.T, exp any, got any, message string) bool {
 		return false
 	}
 	return true
+}
+
+func scanRowsToMapInterface(fields []qry.Field, rows *sql.Rows) ([]map[string]interface{}, error) {
+	defer func(rows *sql.Rows) {
+		_ = rows.Close()
+	}(rows)
+
+	res := make([]map[string]interface{}, 0)
+	for rows.Next() {
+		row, err := scanRowToMapInterface(fields, rows)
+		if err != nil {
+			return res, err
+		}
+		res = append(res, row)
+	}
+
+	return res, nil
+}
+
+func scanRowToMapInterface(fields []qry.Field, scanner qry.Scanner) (map[string]interface{}, error) {
+	numColumns := len(fields)
+	columns := make([]string, numColumns)
+	for index, column := range fields {
+		columns[index] = string(column)
+	}
+
+	values := make([]any, numColumns)
+	valuePointers := make([]any, numColumns)
+	for k := range values {
+		valuePointers[k] = &values[k]
+	}
+
+	if err := scanner.Scan(valuePointers...); err != nil {
+		return nil, fmt.Errorf("could not scan row: %v", err)
+	}
+
+	got := make(map[string]any)
+
+	for i, column := range columns {
+		got[column] = values[i]
+	}
+
+	return got, nil
 }
